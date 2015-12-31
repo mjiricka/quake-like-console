34a73a5b1e0750145aec95564134cf0a093639c6
Martin Jiřička <mjiricka@gmail.com>
2016-01-01 00:32:59 +0100

    new: Now a massive rewritting was done.

--- quake_console.py
+++ quake_console.py
@@ -1,209 +1,189 @@
 #!/usr/bin/env python3
 
+"""This script implements quake-like console in Openbox."""
+
 from math import floor
 from subprocess import Popen
 from time import sleep
-from Xlib.display import Display
-from Xlib import X, Xatom
+
 from ewmh import EWMH
 
+from Xlib.display import Display
+from Xlib import X
+from Xlib import Xatom
+from Xlib.protocol.event import CreateNotify
+#from Xlib.protocol.event import CreateNotify, ReparentNotify, ClientMessage
 
-CONSOLE_PROPERTY_NAME = 'quake_console'
-SLEEP_TIME = 0.03 # How much time in second to wait until terminal opens its window.
-MAX_ATTEMPTS = 10 # How many times try to look for opened terminal.
+
+MAPPED = 0
+UNMAPPED = 1
+
 CONSOLE_HEIGHT_RATIO = 0.38 # = Golden ratio.
 
+CONSOLE_PROP_NAME = 'quake_console'
+CONSOLE_GEOMETRY_PROP_NAME = 'quake_console_geometry'
+CONSOLE_WINDOW_ID_PROP_NAME = 'quake_console_window_id'
 
-# state: 1 = mapped, 0 = unmapped
-def setConsoleProperty(console, state):
-    console.change_property(console_property, Xatom.CARDINAL, 8, [state])
+DISPLAY = Display()
+ROOT = DISPLAY.screen().root
+E = EWMH()
+
+CONSOLE_PROP = DISPLAY.intern_atom(CONSOLE_PROP_NAME)
+CONSOLE_GEOMETRY_PROP = DISPLAY.intern_atom(CONSOLE_GEOMETRY_PROP_NAME)
+CONSOLE_WINDOW_ID_PROP = DISPLAY.intern_atom(CONSOLE_WINDOW_ID_PROP_NAME)
 
 
-def setAppearance(console, alsoGeometry):
+def set_appearance(window):
+    """Makes terminal's window look like a quake console."""
+
+    # This function is not very nice. I experimentally found how
+    # call map, flush, unmap and other things to achieve displaying
+    # without blinking.
+
+    window.unmap()
+    DISPLAY.flush()
+
     # States: 0: remove, 1: add, 2: toggle.
-    ewmh.setWmState(console, 1, '_OB_WM_STATE_UNDECORATED')
-    ewmh.setWmState(console, 1, '_NET_WM_STATE_ABOVE')
-    ewmh.setWmDesktop(console, 0xFFFFFFFF) # All desktops.
+    E.setWmState(window, 1, '_OB_WM_STATE_UNDECORATED')
+    E.setWmState(window, 1, '_NET_WM_STATE_ABOVE')
+    E.setWmDesktop(window, 0xFFFFFFFF) # All desktops.
 
-    if alsoGeometry:
-        ewmh.setWmState(console, 1, '_NET_WM_STATE_MAXIMIZED_HORZ')
-        height = floor(ewmh.getDesktopGeometry()[1] * CONSOLE_HEIGHT_RATIO)
-        ewmh.setMoveResizeWindow(console, y=0, h=height)
-    else:
-        setConsoleGeometry(console)
+    height = floor(E.getDesktopGeometry()[1] * CONSOLE_HEIGHT_RATIO)
+    E.setMoveResizeWindow(window, y=0, h=height)
 
-def rememberConsole(console):
-    consoleId = str(console.id)
-    root.change_property(console_pid, Xatom.STRING, 8, consoleId)
+    # It must be set here.
+    E.setWmState(window, 1, '_NET_WM_STATE_MAXIMIZED_HORZ')
 
-def getConsole():
+    # I do not know why, but this must be called to make all preceding
+    # commands work. I do not understand it :(
+    E.getDesktopGeometry()
+
+    DISPLAY.flush()
+    sleep(0.10)
+    window.map()
+
+
+def save_console_state(window, state):
+    """Saves whether console is mapped (state=MAPPED)
+       or unmapped (state=UNMAPPED)."""
+
+    window.change_property(CONSOLE_PROP, Xatom.CARDINAL, 8, [state])
+
+
+def get_console_state(window):
+    """Gets previously saved state."""
+    prop = window.get_full_property(CONSOLE_PROP, Xatom.CARDINAL)
+    return ord(prop.value[0])
+
+
+def save_console(window):
+    """Saves console's window ID into root's window property."""
+
+    window_id = str(window.id)
+    ROOT.change_property(CONSOLE_WINDOW_ID_PROP, Xatom.STRING, 8, window_id)
+
+
+def get_console():
+    """Gets previously saved console's window (or None)."""
+
     try:
-        consoleId = root.get_full_property(console_pid, Xatom.STRING).value
-        c = display.create_resource_object('window', int(consoleId))
-        prop = c.get_full_property(console_property, Xatom.CARDINAL)
-        value = ord(prop.value[0])
-        return (c, value)
+        # Get window.
+        window_id_str = ROOT.get_full_property(
+            CONSOLE_WINDOW_ID_PROP, Xatom.STRING).value
+        window = DISPLAY.create_resource_object('window', int(window_id_str))
+
+        # Also look for the state. This is useful because we check
+        # that it is really console window. (It is very unlikely, but
+        # somebody could close console and its window ID could get
+        # another window.)
+        state = get_console_state(window)
+        return window, state
     except:
-        return None
-
-def getWindowByPid(pid):
-    windowIds = root.get_full_property(
-        display.intern_atom('_NET_CLIENT_LIST'), X.AnyPropertyType).value
-
-    # Find window with given PID.
-    for windowId in windowIds:
-        window = display.create_resource_object('window', windowId)
-        prop = window.get_full_property(
-            display.intern_atom('_NET_WM_PID'), X.AnyPropertyType)
-        if prop != None:
-            windowPid = prop.value[0]
-            if windowPid == pid:
-                return window
-
-    return None # Window not found.
+        # Nothing relevant was found.
+        return None, None
 
 
-def waitForResult(f, param):
-    result = None
-    attempt = 0
-    while (result is None) and (attempt < MAX_ATTEMPTS):
-        sleep(SLEEP_TIME)
-        result = f(param)
-        attempt += 1
-    return result
+def store_console_geometry(window):
+    """Stores current console position and size into window property."""
+
+    geo = window.get_geometry()
+    # We want absolute coordinates on a screen.
+    tgeo = window.translate_coords(ROOT, geo.x, geo.y)
+    # Serialize geometry and save it.
+    geo_str = '{0};{1};{2};{3}'.format(
+        abs(tgeo.x), abs(tgeo.y), geo.width, geo.height)
+    window.change_property(CONSOLE_GEOMETRY_PROP, Xatom.STRING, 8, geo_str)
 
 
-def searchWindows(window, consoles):
-    children = window.query_tree().children
-    for w in children:
-        prop = w.get_full_property(console_property, Xatom.CARDINAL)
-        if not prop is None:
-            value = ord(prop.value[0])
-            consoles.append((w, value))
-        searchWindows(w, consoles)
+def restore_console_geometry(window):
+    """Restores last console position and size into window property.
+
+       Function must be called after store_console_geometry."""
+
+    geo_str = window.get_full_property(
+        CONSOLE_GEOMETRY_PROP, Xatom.STRING).value
+    # Deserialize the string.
+    geo_str = [int(g) for g in geo_str.split(';')]
+    window.configure(
+        x=int(geo_str[0]),
+        y=int(geo_str[1]),
+        width=int(geo_str[2]),
+        height=int(geo_str[3]))
 
 
-def getConsoles():
-    c = getConsole()
+def start_new_console():
+    """Starts a new terminal, returns its window handle."""
 
-    if c is None:
-        consoles = []
-        searchWindows(root, consoles)
-        return consoles
-    else:
-        print('yeah!')
-        return [c]
-
-
-def showError(errorMsg):
-    Popen(['xmessage', '-center', errorMsg])
-
-
-def saveConsoleGeometry(console):
-    geometry = console.get_geometry()
-    ooo = console.translate_coords(root, geometry.x, geometry.y)
-    geometryStr = (str(-ooo.x)+';'+str(-ooo.y)+';')+ ';'.join(
-        [str(getattr(geometry, p)) for p in ['width', 'height']])
-    console.change_property(console_geometry, Xatom.STRING, 8, geometryStr)
-    print('save', geometryStr)
-
-
-def setConsoleGeometry(console):
-    geometryStr = console.get_full_property(console_geometry, Xatom.STRING).value
-    print('set', geometryStr)
-    geometryStr = [int(g) for g in geometryStr.split(';')]
-    console.configure(
-        x=int(geometryStr[0]),
-        y=int(geometryStr[1]),
-        width=int(geometryStr[2]),
-        height=int(geometryStr[3]))
-
-
-def mapConsolePreserveSize(console):
-    geometry = console.get_geometry()
-    console.map()
-    console.configure(
-        x=geometry.x,
-        y=geometry.y,
-        width=geometry.width,
-        height=501)
-
-
-
-ewmh = EWMH()
-display = ewmh.display
-root = display.screen().root
-console_property = display.intern_atom(CONSOLE_PROPERTY_NAME)
-console_geometry = display.intern_atom('console_geometry') # TODO
-console_pid = display.intern_atom('console_pid_cache') # TODO
-
-
-
-consoles = getConsoles()
-consolesLen = len(consoles)
-
-if consolesLen == 0:
-    # Console is not running.
+    # Listen for events.
+    ROOT.change_attributes(event_mask=X.SubstructureNotifyMask)
 
     # Start the console.
     pid = Popen(['urxvt']).pid
-    console = waitForResult(getWindowByPid, pid)
-    attr = console.get_attributes()
 
-    rememberConsole(console)
+    # Wait for window.
+    for _ in range(1, 1000):
+        event = DISPLAY.next_event()
+        if isinstance(event, CreateNotify) is True:
+            prop = event.window.get_full_property(
+                DISPLAY.intern_atom('_NET_WM_PID'), X.AnyPropertyType)
+            if prop is not None:
+                window_pid = prop.value[0]
+                if window_pid == pid:
+                    return event.window
+    show_error('Unfortunately console did not start.')
 
-    print(vars(attr))
-    print(vars(console.get_wm_hints()))
-    geo = console.get_geometry()
-    print(vars(console.get_geometry()))
-    print(vars(console.get_geometry().root.get_geometry()))
-    ooo = console.translate_coords(root, geo.x, geo.y)
-    print()
-    print (ooo)
-    ooo2 = console.translate_coords(geo.root, geo.x, geo.y)
-    print()
-    print (ooo2)
+
+def show_error(error_msg):
+    """Displays error to user."""
+    Popen(['xmessage', '-center', error_msg])
+
+
+def main():
+    """Module entry."""
+
+    console, state = get_console()
 
     if console is None:
-        showError('Error: Starting the console was not successful!')
+        console = start_new_console()
+        set_appearance(console)
+        save_console_state(console, MAPPED)
+        save_console(console)
     else:
-        print(console.id)
+        if state is MAPPED:
+            store_console_geometry(console)
+            console.unmap()
+            save_console_state(console, UNMAPPED)
+        elif state is UNMAPPED:
+            console.map()
+            restore_console_geometry(console)
+            save_console_state(console, MAPPED)
+        else:
+            show_error('Console data were corrupted!')
 
-        # Console is running - configure it.
-        setAppearance(console, True)
-        # Mark it and say it is mapped.
-        setConsoleProperty(console, 1)
-        # Focus it.
-        ewmh.setActiveWindow(console)
-        saveConsoleGeometry(console)
+main()
 
-elif consolesLen == 1:
-    # Console is running.
-
-    print('conzolka:')
-    print (getConsole())
-
-    (console, value) = consoles[0]
-
-    if value == 1:
-        # Console is mapped. Unmap it.
-        setConsoleProperty(console, 0)
-        saveConsoleGeometry(console)
-        console.unmap()
-    else:
-        # Console is unmapped. Map it.
-        setConsoleProperty(console, 1)
-        #mapConsolePreserveSize(console)
-        console.map()
-        # Configure it again, because it seems many properties
-        # are lost after unmapping...
-        setAppearance(console, False)
-        # And focus it.
-        ewmh.setActiveWindow(console)
-
-else:
-    # This should not happen...
-    showError('Error: More than one console found!')
+# This is very important!
+DISPLAY.flush()
 
 
-display.flush()
+# EOF
