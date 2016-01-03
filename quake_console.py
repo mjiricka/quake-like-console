051814d82e93deafb103c91226755b0f90254a60
Martin Jiřička <mjiricka@gmail.com>
2016-01-03 12:10:04 +0100

    new: Blinking removed plus some other changes that proved to work well.

--- quake_console.py
+++ quake_console.py
@@ -4,7 +4,6 @@
 
 from math import floor
 from subprocess import Popen
-from time import sleep
 
 from ewmh import EWMH
 
@@ -12,7 +11,6 @@ from Xlib.display import Display
 from Xlib import X
 from Xlib import Xatom
 from Xlib.protocol.event import CreateNotify
-#from Xlib.protocol.event import CreateNotify, ReparentNotify, ClientMessage
 
 
 MAPPED = 0
@@ -33,34 +31,25 @@ CONSOLE_GEOMETRY_PROP = DISPLAY.intern_atom(CONSOLE_GEOMETRY_PROP_NAME)
 CONSOLE_WINDOW_ID_PROP = DISPLAY.intern_atom(CONSOLE_WINDOW_ID_PROP_NAME)
 
 
-def set_appearance(window):
+def set_appearance(window, also_geometry):
     """Makes terminal's window look like a quake console."""
-
-    # This function is not very nice. I experimentally found how
-    # call map, flush, unmap and other things to achieve displaying
-    # without blinking.
-
-    window.unmap()
-    DISPLAY.flush()
 
     # States: 0: remove, 1: add, 2: toggle.
     E.setWmState(window, 1, '_OB_WM_STATE_UNDECORATED')
     E.setWmState(window, 1, '_NET_WM_STATE_ABOVE')
     E.setWmDesktop(window, 0xFFFFFFFF) # All desktops.
 
-    height = floor(E.getDesktopGeometry()[1] * CONSOLE_HEIGHT_RATIO)
-    E.setMoveResizeWindow(window, y=0, h=height)
+    if also_geometry:
+        height = floor(E.getDesktopGeometry()[1] * CONSOLE_HEIGHT_RATIO)
+        E.setMoveResizeWindow(window, y=0, h=height)
 
-    # It must be set here.
-    E.setWmState(window, 1, '_NET_WM_STATE_MAXIMIZED_HORZ')
+        # It must be set here.
+        E.setWmState(window, 1, '_NET_WM_STATE_MAXIMIZED_HORZ')
 
     # I do not know why, but this must be called to make all preceding
-    # commands work. I do not understand it :(
+    # commands work. Probably it magically helps to "digest" all
+    # X11 commands when they are mixed with commands from uRXVt itself.
     E.getDesktopGeometry()
-
-    DISPLAY.flush()
-    sleep(0.10)
-    window.map()
 
 
 def save_console_state(window, state):
@@ -161,11 +150,16 @@ def show_error(error_msg):
 def main():
     """Module entry."""
 
+    # I call set_appearance always twice, because for some reasons
+    # I do not understand it is needed to work. At least for me
+    # this way it works reliably.
+
     console, state = get_console()
 
     if console is None:
         console = start_new_console()
-        set_appearance(console)
+        set_appearance(console, also_geometry=True)
+        set_appearance(console, also_geometry=True)
         save_console_state(console, MAPPED)
         save_console(console)
     else:
@@ -175,7 +169,9 @@ def main():
             save_console_state(console, UNMAPPED)
         elif state is UNMAPPED:
             console.map()
+            set_appearance(console, also_geometry=False)
             restore_console_geometry(console)
+            set_appearance(console, also_geometry=False)
             save_console_state(console, MAPPED)
         else:
             show_error('Console data were corrupted!')
